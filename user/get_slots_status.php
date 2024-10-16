2711b7a30e4d5429fea30fdce843dbf42c2c2380
melvingolpio <202101267@cityofmalabonuniversity.edu.ph>
2024-10-17 01:38:33 +0800

    Update get_slots_status.php

--- user/get_slots_status.php
+++ user/get_slots_status.php
@@ -1,13 +1,14 @@
 <?php
 require('../dbconn.php');
 
-$query = "SELECT slot_number, status FROM reservations";
+// Make sure to fetch slot data for all slots, not just one user
+$query = "SELECT slot_number, status FROM reservations";  // Fetch status for all slots
 $result = $conn->query($query);
 
 $statuses = [];
 while ($row = $result->fetch_assoc()) {
-    $statuses[] = $row;
+    $statuses[] = $row;  // Store all slot data in an array
 }
 
-echo json_encode($statuses);
+echo json_encode($statuses);  // Return JSON response for all slots
 ?>

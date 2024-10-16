0678d17989c2c8de77aeb6926546dbddeeded3df
melvingolpio <202101267@cityofmalabonuniversity.edu.ph>
2024-10-16 20:54:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -65,7 +65,7 @@ if (isset($_POST['signin'])) {
 </head>
 <body>
     <div class="wrapper">
-        <form action="login.php" method="post">
+        <form action="index.php" method="post">
             <h1 class="h2">Login</h1>
             <div class="input-box">
                 <input type="text" name="username" placeholder="Username" required>

525927d44db729c5d8669884f7e48f4be59dd189
melvingolpio <202101267@cityofmalabonuniversity.edu.ph>
2024-10-16 20:53:19 +0800

    Update dbconn.php

--- dbconn.php
+++ dbconn.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "pms";
+$servername = "us-cluster-east-01.k8s.cleardb.net";
+$username = "b5f6a402460fa3";
+$password = "83f06a6b";
+$dbname = "heroku_706906bb621a740";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
